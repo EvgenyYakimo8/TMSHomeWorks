045a7014a6b18cf18e3b4f53a858b2dd3f25e09a
EvgenyYakimo8 <colos0117@gmail.com>
2024-06-27 13:47:35 +0300

    test

--- src/main/java/by/tms/lesson21Calculator/Main.java
+++ src/main/java/by/tms/lesson21Calculator/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         ConsoleApplication consoleApplication = new ConsoleApplication();
         consoleApplication.run();
+        System.out.println();
     }
 }
